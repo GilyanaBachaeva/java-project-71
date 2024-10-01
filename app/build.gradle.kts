6f07493b4c7a47a4cc482b2171cdc2ab66ab797f
Gilyana Bachaeva <gelchik29@icloud.com>
2024-10-01 23:50:10 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     application
     checkstyle
@@ -12,37 +15,33 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-
-    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.2")
-    implementation ("info.picocli:picocli:4.7.6")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.11.3")
-    implementation ("org.apache.commons:commons-lang3:3.12.0")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation ("org.junit.platform:junit-platform-suite:1.8.1")
+    testImplementation ("org.skyscreamer:jsonassert:1.5.1")
+    implementation ("info.picocli:picocli:4.7.0")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.8.9")
+    implementation ("io.vertx:vertx-core:3.5.3")
+    testImplementation ("org.assertj:assertj-core:3.24.2")
+    implementation ("org.slf4j:slf4j-simple:2.0.5")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
+    implementation ("commons-io:commons-io:2.6")
 }
 
+application { mainClass.set("hexlet.code.App") }
+
+repositories { mavenCentral() }
 tasks.test {
     useJUnitPlatform()
-}
-
-application {
-    mainClass = "hexlet.code.App"
-}
-
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required = true
-        csv.required = false
-        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
     }
 }
 
-jacoco {
-    toolVersion = "0.8.11"
-    reportsDirectory = layout.buildDirectory.dir("reports/jacoco")
-}
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
