35e20d3f7673e2ac91b6c71f17fe50a9c6f3f60f
Gilyana Bachaeva <gelchik29@icloud.com>
2024-10-02 22:44:42 +0300

    bla bla

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,46 +2,50 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    application
     checkstyle
+    application
     jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.50.0"
 }
 
 group = "hexlet.code"
+
 version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    testImplementation ("org.junit.jupiter:junit-jupiter-params:5.9.2")
-    testImplementation ("org.junit.platform:junit-platform-suite:1.8.1")
-    testImplementation ("org.skyscreamer:jsonassert:1.5.1")
-    implementation ("info.picocli:picocli:4.7.0")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.8.9")
-    implementation ("io.vertx:vertx-core:3.5.3")
-    testImplementation ("org.assertj:assertj-core:3.24.2")
-    implementation ("org.slf4j:slf4j-simple:2.0.5")
-    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
-    implementation ("commons-io:commons-io:2.6")
-}
 
 application { mainClass.set("hexlet.code.App") }
 
 repositories { mavenCentral() }
+
+dependencies {
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.8.9")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
+    implementation("io.vertx:vertx-core:3.5.3")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+}
+
 tasks.test {
     useJUnitPlatform()
-    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
-    testLogging {
-        exceptionFormat = TestExceptionFormat.FULL
-        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-        // showStackTraces = true
-        // showCauses = true
-        showStandardStreams = true
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
     }
 }
 
-tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+
+
+
+
+
