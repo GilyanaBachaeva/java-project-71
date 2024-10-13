f2a1ebab1c1aa0ee427949c2ad7cbe7026d601e2
Gilyana Bachaeva <gelchik29@icloud.com>
2024-10-13 12:05:03 +0300

    some changes at checking 6

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -1,7 +1,6 @@
 package hexlet.code.formatters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.IOException;
 import java.util.List;
